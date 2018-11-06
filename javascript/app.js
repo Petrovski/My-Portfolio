34ee55b9eec4c0d8b686021c9d703a61d68fc01e
FORTRESS <alexpetroski@gmail.com>
2018-11-06 15:38:59 -0500

    Added new js file for other code, seperate from the particles file

---
+++ javascript/app.js
@@ -0,0 +1,3 @@
+$("#viewmywork").on("click", function(){
+    console.log("You clicked the view my work button!!!!!!!");
+});
--- javascript/particles.js
+++ javascript/particles.js
@@ -1,3 +1,4 @@
+
 // Particles background
 
 particlesJS("particles-js", {
